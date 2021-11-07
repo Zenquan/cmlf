8aa87294ec059c2bee8242e413be37d8852468d3
zenquan <1847867150@qq.com>
2021-11-07 21:04:04 +0800

    init: 初始化项目

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import 'tailwindcss/tailwind.css'
+import { ChakraProvider } from "@chakra-ui/react"
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
